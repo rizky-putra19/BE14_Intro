690934f21d69ff78f17b5c06926175a7f2675862
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-08-23 04:52:44 +0700

    first commit [Rizky]

---
+++ first.js
@@ -0,0 +1,21 @@
+// let teks = "Halo"
+// var nama = "Rizky"
+
+// console.log(`${teks} ${nama}`)
+
+
+// const kelas = 10
+
+// console.log (`kelas: ${kelas}`)
+
+
+// const dataOrang = ["Rizky", 10, {nama: "Rizky", kelas: 10}]
+
+// console.log(dataOrang)
+
+
+const object = {kelas: ["frontend", "backend", "mobile"]}
+
+console.log(object)
+
+console.log(object.kelas[2])
